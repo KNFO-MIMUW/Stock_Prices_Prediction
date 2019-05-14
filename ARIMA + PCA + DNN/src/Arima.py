2cf2d442417789eff783c2b029e263e963d2a39e
dburczyk <45153530+dburczyk@users.noreply.github.com>
2019-05-14 06:00:02 +0200

    Update Arima.py

--- ARIMA + PCA + DNN/src/Arima.py
+++ ARIMA + PCA + DNN/src/Arima.py
@@ -1,5 +1,4 @@
 # Authors: Damian Burczyk, Grzegorz Pielot
-# Based on Based on "Stock prediction using deep learning" by Ritika Singh and Shashi Srivastava
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
